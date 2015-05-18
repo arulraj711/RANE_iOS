3ec7e8e998831cd0e6d32718d29373f6e5821d4b
Arul <arul.raj@capestart.com>
2015-05-18 18:13:49 +0530

    Custom Color Changes

--- RATableViewCell.m
+++ RATableViewCell.m
@@ -19,6 +19,7 @@
 //
 
 #import "RATableViewCell.h"
+#import "FIUtils.h"
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
@@ -41,7 +42,10 @@
     
     self.countLabel.layer.masksToBounds = YES;
     self.countLabel.layer.cornerRadius = 13;
-          
+    
+    
+    NSString *menuBackgroundColor = [[NSUserDefaults standardUserDefaults]objectForKey:@"menuBgColor"];
+    [self.contentView setBackgroundColor: [FIUtils colorWithHexString:menuBackgroundColor]];
 }
 
 - (void)prepareForReuse
