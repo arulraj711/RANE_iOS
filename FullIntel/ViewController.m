34fc58b1997af9737eea4c5ec23a1c93ab940276
arulraj <gaugarinoliver@Gaugarin-Olivers-MacBook-Pro.local>
2016-06-22 19:00:49 +0530

    fixed issues in login page and custom messages

--- FullIntel/ViewController.m
+++ FullIntel/ViewController.m
@@ -40,6 +40,8 @@
     originalOuterViewy =_outerView.frame.origin.y;
     [self animateImages];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(afterLogin:) name:@"Login" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(afterLoginFailed) name:@"LoginFailed" object:nil];
+    
     oldFrame = self.backgroundImageView.frame;
     self.originalCenter = self.view.center;
     self.usernameTextField.layer.borderWidth = 1.0f;
@@ -126,6 +128,10 @@
     }
 }
 
+-(void)afterLoginFailed {
+    [activityIndicator stopAnimating];
+    [activityIndicator removeFromSuperview];
+}
 
 
 -(void)viewDidAppear:(BOOL)animated {
